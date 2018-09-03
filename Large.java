1707c2163c2ecf7f26cb6fe48e62bd3ee2cc14aa
atchu1 <atchayaraj18@gmail.com>
2018-09-03 14:05:57 +0530

    Create Large.java

---
+++ Large.java
@@ -0,0 +1,19 @@
+public class Large
+{
+public static void main(String args[])
+{
+int n1=1,n2=2,n3=3;
+if(n1>n2&&n1>=n3)
+{
+System.out.println(num1);
+}
+elseif(n2>=n1&&n2>=n3)
+{
+System.out.println(num2);
+}
+else
+{
+System.out.println(num3);
+}
+}
+}
