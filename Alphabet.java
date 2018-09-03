edc6b0ee61641ba1a60e8bb5cd31ae420baed059
atchu1 <atchayaraj18@gmail.com>
2018-09-03 13:53:29 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,11 @@
+public class Alphabet
+{
+public static void main(String args[])
+{
+char c='a';
+if(c>='a'&&c<='z')
+System.out.println("Alphabet");
+else
+System.out.println("Not anAlphabet");
+}
+}
